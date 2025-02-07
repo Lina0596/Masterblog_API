cd1bab49e1deced4b91e0a1be31f9698e18d32c6
Lina0596 <lina.dahlhaus@icloud.com>
2025-02-07 22:37:05 +0100

    create add post route, functions validate_post_data & find_post_by_id

--- backend/backend_app.py
+++ backend/backend_app.py
@@ -1,8 +1,8 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from flask_cors import CORS
 
 app = Flask(__name__)
-CORS(app)  # This will enable CORS for all routes
+CORS(app)
 
 POSTS = [
     {"id": 1, "title": "First post", "content": "This is the first post."},
@@ -10,9 +10,38 @@ POSTS = [
 ]
 
 
+def validate_post_data(data):
+    if "title" not in data or "content" not in data:
+        return False
+    return True
+
+
+def find_post_by_id(post_id):
+    for i in range(len(POSTS)):
+        if post_id == POSTS[i]["id"]:
+            post = POSTS[i]
+            return post
+    return None
+
+
 @app.route('/api/posts', methods=['GET'])
 def get_posts():
     return jsonify(POSTS)
+
+
+@app.route('/api/posts', methods=['GET', 'POST'])
+def add_post():
+    if request.method == 'POST':
+        new_post = request.get_json()
+        if not validate_post_data(new_post):
+            return jsonify({"error": "Data in title or content is missing"}), 400
+        new_id = max([post["id"] for post in POSTS]) + 1
+        new_post["id"] = new_id
+        POSTS.append(new_post)
+        print(POSTS)
+        return jsonify(new_post), 201
+    else:
+        return jsonify(POSTS)
 
 
 if __name__ == '__main__':
